aa36ff1de9a9143540e483631cdace4b4847f646
treug0lnik041 <mark.jedi@outlook.com>
2021-07-12 00:51:02 +0300

    Update Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -84,10 +84,12 @@ void Minecraft::Application::run()
 			};
 		}
 
-		this->window->clear();
-		this->window->display();
-
 		//Print log information
 		this->logger.flush();
+
+
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		this->window->display();
 	}
 }
